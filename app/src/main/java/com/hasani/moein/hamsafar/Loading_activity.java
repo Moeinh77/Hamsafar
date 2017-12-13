925f00d4cee9c7d48fe9ad92da9c88a0789f5e66
Moeinh77 <moeinhasani77@gmail.com>
2017-12-13 15:49:16 +0330

    Initial commit

---
+++ app/src/main/java/com/hasani/moein/hamsafar/Loading_activity.java
@@ -0,0 +1,13 @@
+package com.hasani.moein.hamsafar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loading_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_activity);
+    }
+}
